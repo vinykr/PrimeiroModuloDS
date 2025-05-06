7a183a09b36e9c0d6cc6ce2dae9f92b99b399ddd
Vinicius Matos <viniciusmatos3333@gmail.com>
2025-05-05 22:20:04 -0300

    Add files via upload

---
+++ Algoritimos05-05/index.js
@@ -0,0 +1,8 @@
+let n1 = number(prompt("Digite o primeiro valor: "));
+let n2 = number(prompt("Digite o segundo valor: "));
+let n3 = number(prompt("Digite o terceiro valor: "));
+
+if (n1> n2 && n1> n3)
+console.log("O maior numero é: ",n1) {
+    else ())
+}
